dea27da9cfcf2a8c500f97f64591ff474b925fd4
Adam <adam@greenough.me>
2024-01-04 16:37:49 +0000

    Local storage basics

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1,5 +1,14 @@
 const editor = document.getElementById('editor');
 
 function init() {
-    editor.value = 'Hello world!';
+    if(localStorage.getItem('page1') === null) {
+        localStorage.setItem('page1', 'Hello world!');
+    }
+
+    editor.value = localStorage.getItem('page1');
+    editor.focus();
+}
+
+editor.oninput = function() {
+    localStorage.setItem('page1', editor.value);
 }
